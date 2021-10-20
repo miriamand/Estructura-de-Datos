5fd358203e8e42d4d1279e6b6064c11014452fb2
miriamand <80232472+miriamand@users.noreply.github.com>
2021-10-20 14:55:16 -0500

    Add files via upload

---
+++ practica5/js/clase2.js
@@ -0,0 +1,10 @@
+import clase1 from "./clase1.js";
+let clase1 = new clase1(); 
+
+class clase2 extends clase1{
+    constructor(){
+        super();
+    }
+} 
+
+export default clase2;
---
+++ practica5/js/index.js
@@ -0,0 +1,4 @@
+import clase2 from "./clase2.js";
+let clase2 = new clase2(); 
+
+console.log(clase2.propiedad);
